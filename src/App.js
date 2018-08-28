2e5a9d7395f14966b94e94bb0f51a460e66617ef
vadimkotliarchuk <vadimkotliarchuk@gmail.com>
2018-08-28 04:50:42 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { LoginForm } from './LoginForm.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Ti-app</h1>
+          <button class="common-btn" onClick={()=> console.log('clicked')}>login</button>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <LoginForm></LoginForm>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/LoginForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class LoginForm extends Component {
+    render () {
+        return (
+            <div>Login</div>
+        );
+    }
+}
+
+export default LoginForm;
