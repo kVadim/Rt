365941691120ccd5bb722cea1b4c5c686266f647
vadimkotliarchuk <vadimkotliarchuk@gmail.com>
2018-10-22 01:47:10 +0300

    project new structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import LoginForm  from './LoginForm.js'
-import Doer  from './components/doer.js'
+import { BrowserRouter, Switch, Route  } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Ti-app</h1>
-          <button className="common-btn" onClick={()=> console.log('clicked')}>login</button>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <LoginForm/>
-        <Doer/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />  
+          <Switch>
+            <Route path='/' component={Dashboard} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return (
+        <div className="card z-depth-0 project-summary">
+            <div className="card-conternt grey-text text-darken-3">
+                <span className="card-title">Title</span>
+                <p>Posted by me</p>
+                <span className="grey-text">3rd September 2pm</span>
+            </div>
+        </div> 
+    )
+}
+
+export default ProjectSummary;
