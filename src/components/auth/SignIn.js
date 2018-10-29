e29cccae6c322ef5f4ae015337f491cdf42d1475
vadimkotliarchuk <vadimkotliarchuk@gmail.com>
2018-10-29 18:40:06 +0200

    SignIn component

---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class SignIn extends Component {
+state =  {
+    
+}
+  render() {
+    return (
+        <div className="container">
+            <form className="white" onSubmit={}>
+                <h5 className="grey-text text-darken-3">Sign In</h5>
+            </form>
+        </div>
+    )
+  }
+}
+
+export default SignIn
