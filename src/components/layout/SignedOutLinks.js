6e985e61305a19381e62cc00d603eb5e2378b8da
vadimkotliarchuk <vadimkotliarchuk@gmail.com>
2018-10-31 09:24:02 +0200

    signIn & signUp forms

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -5,7 +5,7 @@ const SignedOutLinks = () =>  {
     return (
       <ul className="right">
           <li><NavLink to="/" >Signup</NavLink></li>
-          <li><NavLink to="/" >Login</NavLink></li>
+          <li><NavLink to="/signin" >Login</NavLink></li>
       </ul>
     )
 }
