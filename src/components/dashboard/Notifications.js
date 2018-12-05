8cf6169f895b10cb31b9a8115afa19710f1180b5
vadimkotliarchuk <vadimkotliarchuk@gmail.com>
2018-12-06 00:45:59 +0200

    Notifications refactoring & firebase func init

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,11 +1,19 @@
 import  React from 'react'
 
-const Notifications = () => {
+const Notifications = (props) => {
     return (
-        <div>
-            <p>
-                Notifications
-            </p>
+        <div className="section">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Notifications</span>
+                    <ul className="notifications">
+                        <li>notif</li>
+                        <li>notif</li>
+                        <li>notif</li>
+                        <li>notif</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
